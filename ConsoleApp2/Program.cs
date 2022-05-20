3168d79154ddcac6591af6d70e4ee23b0ef3ef8f
kartikesh.chaudhari <kartikesh.chaudhari@AZ-VDI-D1-251>
2022-05-20 01:39:03 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+   internal class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collection List");
+            ArrayList arrList = new ArrayList();
+            arrList.Add(11);
+            arrList.Add(22);
+            arrList.Add(33);
+            arrList.Add(44);
+
+            arrList.Add(2.5);
+            arrList.Add(3.5);
+            arrList.Add(4.5);
+            arrList.Add(5.5);
+
+
+            arrList.Add("kartik");
+            arrList.Add("satyam");
+            arrList.Add("prajwal");
+            arrList.Add("saurabh");
+
+            arrList.Add('A');
+            arrList.Add('B');
+            arrList.Add('C');
+            arrList.Add('D');
+            int sum = 0;
+            int count = 0;
+            Console.WriteLine($"Length of array list = {arrList.Count}");
+            foreach (object item in arrList)
+            {
+                if (item is int)
+                {
+                    sum = sum + (int)item;
+                    if((int)item%2==0)
+                    {
+                        Console.WriteLine("Even Number = "+(int)item);
+                        count++;
+                        Console.WriteLine();
+                    }
+                }
+                else if(item is string)
+                {
+                    string item1 = (string)item;
+                    //Console.WriteLine("String Name ="+(string)item+" ,String Length =" + item1.Length + " , Index = " + arrList.IndexOf((string)item));
+                    Console.WriteLine($"String Name = {(string)item} ");
+                    Console.WriteLine($"String Length = { item1.Length}");
+                    Console.WriteLine($"Index = { arrList.IndexOf((string)item)}");
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine($"Int sum =" + sum);
+            Console.WriteLine();
+            Console.WriteLine($"Count of  even numbers are { count}");
+            
+
+
+        }
+    }
+}
---
+++ cs_SimpleProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cs_SimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Operation On OOPS");
+            operation op = new operation();
+            double rRaiseTo = op.RaiseTo(10, 3);
+            Console.WriteLine($"RaiseTo of given number are {rRaiseTo}");
+        }
+    }
+    public class operation
+    {
+        private bool Validate(int x, int y)
+        {
+            if (x == 0 || y == 0)
+                return false;
+            else
+                return true;
+        }
+        public double RaiseTo(int x, int y)
+        {
+            if (!Validate(x, y)) return 0;
+            double z = Math.Pow(x, y);
+            return z;
+        }
+
+    }
+}
